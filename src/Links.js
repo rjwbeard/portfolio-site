3244921bcdd9aa08f64aa9f958e352f22d47b320
Rebecca Beard <rbccbeard@gmail.com>
2022-02-04 14:43:54 -0500

    UPDATE: copy and links

--- src/Links.js
+++ src/Links.js
@@ -1,4 +1,5 @@
 import Content from "./content.json";
+import TWITTER from "./images/TWITTER.png";
 
 const Links = () => {
   return (
@@ -6,6 +7,8 @@ const Links = () => {
       <a
         className="app-link"
         id="portfolio-link"
+        target="_blank"
+        rel="noopener noreferrer"
         href="https://github.com/rjwbeard"
       >
         {Content.App.portfolioButton.text}
@@ -13,12 +16,30 @@ const Links = () => {
       <a
         className="app-link"
         id="experience-link"
+        target="_blank"
+        rel="noopener noreferrer"
         href="https://www.linkedin.com/in/rebeccajwbeard/"
       >
         {Content.App.experienceButton.text}
       </a>
-      <a className="app-link" href="/" id="contact-link">
+      <a
+        className="app-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/"
+        id="contact-link"
+      >
         {Content.App.contactButton.text}
+      </a>
+      <a
+        href="https://twitter.com/mythsabouthell?ref_src=twsrc%5Etfw"
+        className="app-link"
+        data-show-count="false"
+        target="_blank"
+        rel="noopener noreferrer"
+        id="twitter-link"
+      >
+        <img src={TWITTER} id="twitter-icon" alt="Twitter logo" />
       </a>
     </div>
   );
